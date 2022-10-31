812b832d36e699ec5aef8896278e8f8d39102db9
Jess <jessbpeck@gmail.com>
2022-10-31 12:23:52 -0700

    Update starrating_amazon.js

--- data-hooks/starrating_amazon.js
+++ data-hooks/starrating_amazon.js
@@ -3,7 +3,10 @@ var star_ratings = []
 var review_date = []
 var count_of_ratings = []
 var reviewerid = []
+var reviewid = []
+let productlist = []
 var reviews = document.querySelectorAll('[id^="customer_review-"]')
+let product = document.title
 
 for (var i = 0; i < reviews.length; i++)
 {
@@ -12,16 +15,19 @@ for (var i = 0; i < reviews.length; i++)
     var reviewer = reviews[i].dataset.celWidget
 var star_rating = reviews[i].querySelector('[data-hook="review-star-rating"]').innerText
 var review = reviews[i].querySelector('[data-hook="review-date"]').innerText
-
+var indivreviewid = reviews[i].dataset.csaCId
 count_of_ratings.push(count)
     reviewerid.push(reviewer)
+    reviewid.push(indivreviewid)
+    console.log(indivreviewid)
 star_ratings.push(star_rating)
 review_date.push(review)
+productlist.push(product)
 
 }
-const zip = (a, b, c) => a.map((k, i) => [k, b[i], c[i]]);
-const extended = zip(star_ratings, review_date, reviewerid)
-extended.unshift(['rating', 'date', 'reviewerid'])
+const zip = (a, b, c, d, e) => a.map((k, i) => [k, b[i], c[i], d[i], e[i]]);
+const extended = zip(star_ratings, review_date, reviewerid, productlist, reviewid)
+extended.unshift(['rating', 'date', 'reviewerid', 'product', 'review_id'])
 
 return {
     cell: reviews.length,
